d609072f2514d9f7e637b9430d76c500634f1e45
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2024-11-18 11:52:53 +0100

    trying to implement search bar

--- app/src/main/java/dk/dtu/ToDoList/feature/TopBar.kt
+++ app/src/main/java/dk/dtu/ToDoList/feature/TopBar.kt
@@ -11,11 +11,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.IconButton
 import androidx.compose.runtime.ComposableOpenTarget
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.graphics.Color
+
 
 @Composable
 fun TopBar(){
+    var isSearchActive by remember { mutableStateOf(false) }
+    var searchText by remember { mutableStateOf("") }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -27,11 +44,28 @@ fun TopBar(){
             contentDescription = "Profile Icon",
             modifier = Modifier.size(32.dp)
         )
-        Image(
-            painter = painterResource(id = R.drawable.search),
-            contentDescription = "Search Icon",
-            modifier = Modifier.size(32.dp)
-        )
+        AnimatedVisibility(
+            visible = isSearchActive,
+            enter = slideInHorizontally(initialOffsetX = { it }) + fadeIn(),
+            exit = slideOutHorizontally(targetOffsetX = { it }) + fadeOut()
+        ) {
+            BasicTextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                modifier = Modifier
+                    .fillMaxWidth(0.7f) // Adjust size as needed
+                    .padding(end = 8.dp),
+                textStyle = TextStyle(color = Color.Black)
+            )
+        }
+        IconButton(onClick = {isSearchActive = !isSearchActive}) {
+            Image(
+                painter = painterResource(id = R.drawable.search),
+                contentDescription = "Search Icon",
+                modifier = Modifier.size(32.dp)
+
+            )
+        }
     }
 }
 
