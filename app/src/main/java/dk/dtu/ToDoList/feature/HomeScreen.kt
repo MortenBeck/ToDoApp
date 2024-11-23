6d0ec6a66bde0c90d53a375aeb24f91b969f9a57
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2024-11-23 16:05:50 +0100

    homescreen

--- app/src/main/java/dk/dtu/ToDoList/feature/HomeScreen.kt
+++ app/src/main/java/dk/dtu/ToDoList/feature/HomeScreen.kt
@@ -1,2 +1,15 @@
 package dk.dtu.ToDoList.feature
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dk.dtu.ToDoList.data.Task
+
+@Composable
+fun HomeScreen(tasks: List<Task>){
+    Column (modifier = Modifier.fillMaxSize()) {
+        TopBar()
+        TaskListScreen(tasks)
+    }
+}
