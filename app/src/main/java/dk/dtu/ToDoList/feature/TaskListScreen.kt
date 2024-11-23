d295f7ecfecfec6b05899e7ccc7f5da8fb6ad71f
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2024-11-23 16:04:38 +0100

    changing tasklistscreen and homescreen

---
+++ app/src/main/java/dk/dtu/ToDoList/feature/TaskListScreen.kt
@@ -0,0 +1,76 @@
+package dk.dtu.ToDoList.feature
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dk.dtu.ToDoList.data.Task
+
+@Composable
+fun TaskListScreen(tasks: List<Task>) {
+    // Getting todays date with calendar
+    val today = java.util.Calendar.getInstance().apply {
+        set(java.util.Calendar.HOUR_OF_DAY, 0)
+        set(java.util.Calendar.MINUTE, 0)
+        set(java.util.Calendar.SECOND, 0)
+        set(java.util.Calendar.MILLISECOND, 0)
+    }.time
+
+    // Making a difference between today and future deadlines!
+    val todayTasks = tasks.filter { it.deadline <= today }
+    val futureTasks = tasks.filter { it.deadline > today }
+
+    LazyColumn (modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)) {
+        // "Today" Section
+        if (todayTasks.isNotEmpty()) {
+            item {
+                SectionHeader(title = "Today")
+            }
+            itemsIndexed(todayTasks){index, task ->
+                TaskItem(task = task)
+            }
+            item{
+                Spacer(modifier = Modifier.height(16.dp)) // Space between sections
+            }
+        }
+
+        // "Future" Section
+        if (futureTasks.isNotEmpty()) {
+            item{
+                SectionHeader(title = "Future")
+            }
+            itemsIndexed(futureTasks){index, task ->
+                TaskItem(task = task)
+            }
+
+        }
+    }
+}
+// Creating an area for the "Today" and "Future" headline
+@Composable
+fun SectionHeader(title: String) {
+    Column {
+        Text(
+            text = title,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            color = Color.Black
+        )
+        Divider(color = Color.Gray, thickness = 1.dp)
+    }
+}
