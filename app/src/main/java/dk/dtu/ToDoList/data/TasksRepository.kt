967f0ff8bcff0b10dd48262d61c257a7e7764a0d
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2024-11-30 14:57:19 +0100

    update task repo

--- app/src/main/java/dk/dtu/ToDoList/data/TasksRepository.kt
+++ app/src/main/java/dk/dtu/ToDoList/data/TasksRepository.kt
@@ -11,77 +11,77 @@ object TasksRepository {
     val Tasks = listOf(
         Task(
             name = "Homework - UX",
-            deadline = simpleDateFormat.parse("17-11-2024")!!,
+            deadline = simpleDateFormat.parse("30-11-2024")!!,
             priority = TaskPriority.HIGH,
             tag = TaskTag.SCHOOL,
             completed = false
         ),
         Task(
             name = "Fix project at work",
-            deadline = simpleDateFormat.parse("17-11-2024")!!,
+            deadline = simpleDateFormat.parse("02-12-2024")!!,
             priority = TaskPriority.HIGH,
             tag = TaskTag.WORK,
             completed = false
         ),
         Task(
             name = "Walk the dog",
-            deadline = simpleDateFormat.parse("17-11-2024")!!,
+            deadline = simpleDateFormat.parse("02-12-2024")!!,
             priority = TaskPriority.MEDIUM,
             tag = TaskTag.PET,
             completed = false
         ),
         Task(
             name = "Cancel Netflix subscription",
-            deadline = simpleDateFormat.parse("17-11-2024")!!,
+            deadline = simpleDateFormat.parse("03-12-2024")!!,
             priority = TaskPriority.LOW,
             tag = TaskTag.HOME,
             completed = false
         ),
         Task(
             name = "Call mechanic",
-            deadline = simpleDateFormat.parse("18-11-2024")!!,
+            deadline = simpleDateFormat.parse("05-12-2024")!!,
             priority = TaskPriority.HIGH,
             tag = TaskTag.TRANSPORT,
             completed = false
         ),
         Task(
             name = "Grocery Shopping",
-            deadline = simpleDateFormat.parse("18-11-2024")!!,
+            deadline = simpleDateFormat.parse("05-12-2024")!!,
             priority = TaskPriority.MEDIUM,
             tag = TaskTag.HOME,
             completed = false
         ),
         Task(
             name = "Reorganize desk at work",
-            deadline = simpleDateFormat.parse("18-11-2024")!!,
+            deadline = simpleDateFormat.parse("05-12-2024")!!,
             priority = TaskPriority.LOW,
             tag = TaskTag.WORK,
             completed = false
         ),
         Task(
             name = "Clean bathroom",
-            deadline = simpleDateFormat.parse("19-11-2024")!!,
+            deadline = simpleDateFormat.parse("06-12-2024")!!,
             priority = TaskPriority.MEDIUM,
             tag = TaskTag.HOME,
             completed = false
         ),
         Task(
             name = "Get ready for album drop",
-            deadline = simpleDateFormat.parse("21-11-2024")!!,
+            deadline = simpleDateFormat.parse("07-12-2024")!!,
             priority = TaskPriority.LOW,
             tag = TaskTag.HOME,
             completed = false
         ),
         Task(
             name = "Homework - Math",
-            deadline = simpleDateFormat.parse("22-11-2024")!!,
+            deadline = simpleDateFormat.parse("09-12-2024")!!,
             priority = TaskPriority.HIGH,
             tag = TaskTag.SCHOOL,
             completed = false
         ),
         Task(
             name = "Find passport",
-            deadline = simpleDateFormat.parse("31-11-2024")!!,
+            deadline = simpleDateFormat.parse("10-12-2024")!!,
             priority = TaskPriority.MEDIUM,
             tag = TaskTag.HOME,
             completed = false
